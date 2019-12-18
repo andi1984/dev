eb7c0a83cf12df8632928e1a97d9abbb8f68c103
Andreas Sander <andreas.sander@hotmail.de>
2019-12-18 22:28:14 +0100

    Move url util to typescript.

--- utils/url.ts
+++ utils/url.ts
@@ -1,6 +1,6 @@
 module.exports = {
   // https://stackoverflow.com/a/38979205/778340
-  isAbsoluteURL: url => {
+  isAbsoluteURL: (url: string) => {
     return url.indexOf("://") > 0 || url.indexOf("//") === 0;
   }
 };
