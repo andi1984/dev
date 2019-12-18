7163926e9f16ae0f2308c84524ebdd8419b69e3d
Andreas Sander <andreas.sander@hotmail.de>
2019-12-18 22:26:42 +0100

    build - Add explicit types.

--- build.ts
+++ build.ts
@@ -2,7 +2,7 @@ const fs = require("fs");
 const MARKDOWN_FOLDER = require("./utils/constants").MARKDOWN_FOLDER;
 const renderMarkdownFile = require("./utils/renderMDFile");
 
-fs.readdir(MARKDOWN_FOLDER, (err, files) => {
+fs.readdir(MARKDOWN_FOLDER, (err: Error, files: string[]) => {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
