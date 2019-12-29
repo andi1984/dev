fba256f17449943b54b613323a4030f922e01bbf
Andreas Sander <andreas.sander@hotmail.de>
2019-12-29 20:42:59 +0100

    feat(utils) - Introduce site interface
    Introduce a new TS interface for sites containing all necessary information about one site. A list
    of all sites is added as template variable to the twig templates.

--- utils/renderMDFile.ts
+++ utils/renderMDFile.ts
@@ -1,88 +1,27 @@
 // https://stackoverflow.com/a/41975448/778340
 export {};
 
+import { Site } from './md';
+
 const path = require("path");
 const fs = require("fs");
 
 const minify = require("html-minifier").minify;
 const Twig = require("twig");
-const isAbsoluteURL = require("./url").isAbsoluteURL;
-const frontmatterUtils = require("./frontmatter");
 
-const supportForPrettyUrls = Boolean(process.env.PRETTY_URLS);
-console.log('Support for pretty URLs', supportForPrettyUrls);
-const MarkdownIt = require("markdown-it"),
-  md = (frontmatterCallback: Function) =>
-    new MarkdownIt({
-      replaceLink: function(link: string): string {
-        return Boolean(process.env.PRETTY_URLS) ||
-          isAbsoluteURL(link) ||
-          link.indexOf(".html") !== -1
-          ? link
-          : `${link}.html`;
-      }
-    })
-      .use(require("markdown-it-replace-link"))
-      .use(require("markdown-it-front-matter"), frontmatterCallback);
-
-const MARKDOWN_FOLDER = require("./constants").MARKDOWN_FOLDER;
 const DISTRIBUTION_FOLDER = require("./constants").DISTRIBUTION_FOLDER;
-const TEMPLATE_FOLDER = require("./constants").TEMPLATE_FOLDER;
 
-module.exports = (absoluteFilePath: string) => {
-  const relativeFileFolderToMDFolder = path.relative(
-    MARKDOWN_FOLDER,
-    path.dirname(absoluteFilePath)
-  );
-  const fileName = path.basename(absoluteFilePath, ".md");
-
-  // Set default template to render
-  let twigTemplate = "./templates/main.twig";
-
-  // Set default page title to the file name
-  let title = fileName;
-
-  // Read in MD file synchronously.
-  const mdString = fs.readFileSync(absoluteFilePath, "utf8");
-  const htmlOutput = md((frontmatter: string): void => {
-    /* HANDLE TEMPLATE FRONTMATTER */
-    const templateFrontmatter = frontmatterUtils.findFrontmatterTag(
-      frontmatter,
-      "template"
-    );
-
-    if (templateFrontmatter) {
-      const templateName = frontmatterUtils.frontmatterValue(
-        templateFrontmatter
-      );
-
-      const targetTemplatePath = path.resolve(TEMPLATE_FOLDER, templateName);
-
-      if (fs.existsSync(targetTemplatePath)) {
-        twigTemplate = targetTemplatePath;
-      }
-    }
-
-    /* HANDLE TITLE FRONTMATTER */
-    const titleFrontmatter = frontmatterUtils.findFrontmatterTag(
-      frontmatter,
-      "title"
-    );
-
-    if (titleFrontmatter) {
-      title = frontmatterUtils.frontmatterValue(titleFrontmatter);
-    }
-  }).render(mdString);
-
+module.exports = async (site: Site, sites: Site[]) => {
   if (!fs.existsSync(DISTRIBUTION_FOLDER)) {
     fs.mkdirSync(DISTRIBUTION_FOLDER);
   }
 
   Twig.renderFile(
-    twigTemplate,
+    site.template,
     {
-      title,
-      content: htmlOutput
+      title: site.title,
+      sites,
+      content: site.html
     },
     (err: Error, html: string) => {
       return new Promise((resolve, reject) => {
@@ -92,19 +31,23 @@ module.exports = (absoluteFilePath: string) => {
 
         const outputFilePath = path.join(
           DISTRIBUTION_FOLDER,
-          relativeFileFolderToMDFolder,
-          `${fileName}.html`
+          site.file.rel,
+          `${site.file.name}.html`
         );
 
         // Create folder in case it doesn't exist already.
         if (!fs.existsSync(path.dirname(outputFilePath))) {
-          fs.mkdirSync(path.dirname(outputFilePath), { recursive: true });
+          fs.mkdirSync(path.dirname(outputFilePath), {
+            recursive: true
+          });
         }
 
         // Write the file into the folder
         fs.writeFile(
           outputFilePath,
-          minify(html, { collapseWhitespace: true }),
+          minify(html, {
+            collapseWhitespace: true
+          }),
           (writeErr: Error) => {
             if (writeErr) {
               reject(writeErr);
--- utils/url.ts
+++ utils/url.ts
@@ -1,8 +1,20 @@
 // https://stackoverflow.com/a/41975448/778340
 export { };
+  
+const isAbsoluteURL = (url: string) => {
+  return url.indexOf("://") > 0 || url.indexOf("//") === 0;
+};
+
+const generateURL = (link: string): string => {
+  return Boolean(process.env.PRETTY_URLS) ||
+    isAbsoluteURL(link) ||
+    link.indexOf(".html") !== -1
+    ? link
+    : `${link}.html`;
+};
+
 module.exports = {
   // https://stackoverflow.com/a/38979205/778340
-  isAbsoluteURL: (url: string) => {
-    return url.indexOf("://") > 0 || url.indexOf("//") === 0;
-  }
+  isAbsoluteURL,
+  generateURL
 };
