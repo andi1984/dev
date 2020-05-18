8a9b62c98c898ca23905548cff26e858a6acac25
Andreas Sander <mail@andi1984.de>
2020-05-18 22:36:52 +0200

    Get rid of back2roots files and use it via a dep.

--- utils/constants.ts
+++
@@ -1,9 +0,0 @@
-// https://stackoverflow.com/a/41975448/778340
-export { };
-const path = require("path");
-
-module.exports = {
-  MARKDOWN_FOLDER: path.resolve("./md"),
-  DISTRIBUTION_FOLDER: path.resolve("./dist"),
-  TEMPLATE_FOLDER: path.resolve("./templates")
-};
