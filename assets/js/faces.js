f2389daf65d5b5dedd1f1675d51f394b2a2323b6
Andreas Sander <mail@andi1984.de>
2020-12-13 18:24:49 +0100

    fix(js): Fix sentry issue with requestIdleCallback incompat browsers (2028674372)

--- assets/js/faces.js
+++ assets/js/faces.js
@@ -3,29 +3,37 @@ const NUMBER_OF_FACES = 70;
 export default () => {
   let idleCallbackRequest, facesElement, loadTimeout, resizeTimeout;
 
-  const setNewBackgroundValues = () => {
-    if (idleCallbackRequest) {
-      window.cancelIdleCallback(idleCallbackRequest);
+  const setFace = () => {
+    facesElement = document.getElementById('faces');
+
+    // If the faces container currently has a background image, then we know
+    // that we are allowed to show a different one, otherwise we do nothing.
+    if (window.getComputedStyle(facesElement).backgroundImage !== 'none') {
+      const faceNr = Math.floor(Math.random() * NUMBER_OF_FACES) + 1;
+      facesElement.style.backgroundImage = `url('/assets/${faceNr}.png')`;
     }
+  };
 
-    idleCallbackRequest = window.requestIdleCallback(() => {
-      facesElement = document.getElementById('faces');
-
-      // If the faces container currently has a background image, then we know
-      // that we are allowed to show a different one, otherwise we do nothing.
-      if (window.getComputedStyle(facesElement).backgroundImage !== 'none') {
-        const faceNr = Math.floor(Math.random() * NUMBER_OF_FACES) + 1;
-        facesElement.style.backgroundImage = `url('/assets/${faceNr}.png')`;
+  const setNewBackgroundValues = () => {
+    if ('requestIdleCallback' in window) {
+      // Use requestIdleCallback to schedule work.
+      if (idleCallbackRequest) {
+        window.cancelIdleCallback(idleCallbackRequest);
       }
-    });
+
+      idleCallbackRequest = window.requestIdleCallback(setFace);
+    } else {
+      // Do what you’d do today.
+      setFace();
+    }
   };
 
   let prevOnLoad;
-  if(typeof window.onload === 'function') {
+  if (typeof window.onload === 'function') {
     prevOnLoad = window.onload;
   }
   window.onload = () => {
-    if(!!prevOnLoad) {
+    if (!!prevOnLoad) {
       prevOnLoad();
     }
 
